409f6420c4e4b9eca0c9d78602b2ebb51d4515f5
FaresMedhat <sssfaresmedhat@gmail.com>
2026-08-26 03:04:30 +0300

    finish data source of  favourit  products

---
+++ lib/favourit_page/data/data_source/favourit_products_remote_data_source.dart
@@ -0,0 +1,16 @@
+import 'package:marketi/core/api/dio_helper.dart';
+import 'package:marketi/favourit_page/data/models/favourit_products_model.dart';
+
+class FavouritProductsRemoteDataSource {
+  Future<List<FavouritProductsModel>> getFavouritProducts() async {
+    final response = await DioHelper.getData(url: "favourits", withAuth: true);
+    // استخراج القائمة القادمة في الـ Response
+    final List dynamicList = response.data['data'] ?? response.data;
+
+    // تحويل الـ JSON إلى Models
+    return dynamicList
+        .map((json) =>
+            FavouritProductsModel.fromJson(json as Map<String, dynamic>))
+        .toList();
+  }
+}
