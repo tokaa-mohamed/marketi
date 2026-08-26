7e8d04988d9c3cbb97597ba1fc8f28b85011fc2c
FaresMedhat <sssfaresmedhat@gmail.com>
2026-08-26 03:55:05 +0300

    finish repositoryy of data layer

---
+++ lib/favourit_page/data/repositories/favourit_products_repository_impl.dart
@@ -0,0 +1,33 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:marketi/core/api/internet_connection_checker.dart';
+import 'package:marketi/core/errors/failure.dart';
+import 'package:marketi/favourit_page/data/data_source/favourit_products_remote_data_source.dart';
+import 'package:marketi/favourit_page/domain/entities/favourit_products_entities.dart';
+import 'package:marketi/favourit_page/domain/repositories/favourit_products_repositories.dart';
+
+class FavouritProductsRepositoryImpl extends FavouritProductsRepositories {
+  final FavouritProductsRemoteDataSource remoteDataSource;
+  final NetworkInfo networkInfo;
+
+  FavouritProductsRepositoryImpl(
+      {required this.remoteDataSource, required this.networkInfo});
+  @override
+  Future<Either<Failure, List<FavouritProductsEntities>>>
+      getFavouritProducts() async {
+    if (await networkInfo.isConnected) {
+      try {
+        final favouritProducts = await remoteDataSource.getFavouritProducts();
+        return Right(favouritProducts);
+      } on DioException catch (e) {
+        final String errorMessage = e.response?.data?['message']?.toString() ??
+            e.message ??
+            'An API error occurred';
+
+        return Left(ApiFailure(message: errorMessage));
+      }
+    } else {
+      return Left(NoInternetFailure(message: 'No Internet Connection'));
+    }
+  }
+}
